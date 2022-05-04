0fed36a64d4dc5cd96b59eae63e4ea3f1242c652
yyj <yyj6336@163.com>
2022-05-04 22:26:41 +0800

    SCVO-with-ssim_depth_loss

---
+++ SCVO/kitti_eval/download_kitti_depth_predictions.sh
@@ -0,0 +1,4 @@
+URL=http://people.eecs.berkeley.edu/~tinghuiz/projects/SfMLearner/predictions/kitti_eigen_depth_predictions.npy
+#OUTPUT_FILE=./kitti_eval/kitti_eigen_depth_predictions.npy
+OUTPUT_FILE=/home/zhq/YYJ/dataset/kitti_eigen_depth_predictions.npy
+wget -N $URL -O $OUTPUT_FILE
